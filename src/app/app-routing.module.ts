60d7aedc25fc0ca7fc91d2e90548113d3e08cd84
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-09-16 15:41:50 +0300

    Added secret page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainPageComponent} from './main-page/main-page.component';
+import {SecretPageComponent} from './secret-page/secret-page.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
+  { path: 'secret', component: SecretPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SecretPageComponent } from './secret-page/secret-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    SecretPageComponent
   ],
   imports: [
     BrowserModule,
