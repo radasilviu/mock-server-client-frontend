19eb581f821d69bd9a2ab1d8394905701df68c66
Robert Kis <robert.kis@fortech.ro>
2020-09-16 20:20:04 +0300

    Partially implemented redirection to auth server login form

--- src/app/configs/sample.env.ts
+++ src/app/configs/sample.env.ts
@@ -1,6 +1,7 @@
 export class SampleEnv {
   static resourceServerRootURL: string;
   static authServerRootURL: string;
+  static authServerLoginFormURL: string;
   static clientId: string;
   static clientSecret: string;
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import * as Rx from 'rxjs';
+import {Env} from '../configs/env';
 
 @Component({
   selector: 'app-main-page',
@@ -9,42 +10,17 @@ import * as Rx from 'rxjs';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
-    this.baseUrl = 'http://localhost:4200/';
-    this.clientId = '1';
-    this.clientSecret = '2';
+  constructor(private route: ActivatedRoute) {}
 
-    // Test-only
-    this.authToken.subscribe((data) => {
-      console.log('authToken: ', data);
-    });
-    this.refreshToken.subscribe((data) => {
-      console.log('refreshToken: ', data);
-    });
-  }
-
-   authToken = new Rx.Subject();
-   refreshToken = new Rx.Subject();
-
-  private baseUrl: string;
-  private clientId: string;
-  private clientSecret: string;
 
   ngOnInit(): void {
     if (this.route.snapshot.queryParams.authToken) {
-      this.authToken.next(this.route.snapshot.queryParams.authToken);
-    }
-    if (this.route.snapshot.queryParams.refreshToken) {
-      this.refreshToken.next(this.route.snapshot.queryParams.refreshToken);
+
     }
   }
 
-  goToUrl(): void {
-    window.location.href = this.baseUrl + '?clientId=' + this.clientId + '&clientSecret=' + this.clientSecret;
-  }
-
-  testTokenSave(): void {
-    window.location.href = 'http://localhost:4200/?authToken=1&refreshToken=2';
+  login(): void {
+    window.location.href = `${Env.authServerLoginFormURL}/oauth/client-login?clientId=${Env.clientId}&clientSecret=${Env.clientSecret}&redirectURL=${window.location.href}`;
   }
 
 }
