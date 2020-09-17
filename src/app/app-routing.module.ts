0c0be5e392b353c884555dee479b304798b76869
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-09-17 11:45:10 +0300

    Added dashboard page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainPageComponent} from './main-page/main-page.component';
 import {SecretPageComponent} from './secret-page/secret-page.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'secret', component: SecretPageComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
