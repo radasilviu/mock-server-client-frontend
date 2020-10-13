2ae89e99f3b59440a1f56e21ff14f2350601ed40
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-10-13 20:36:33 +0300

    Imported Mat-checkbox modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import { EditCompanyComponent } from './components/dialogs/edit-company/edit-company.component';
 import {MatDialogModule} from '@angular/material/dialog';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { DeleteCompanyComponent } from './components/dialogs/delete-company/delete-company.component';
@@ -30,6 +30,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import { BooksComponent } from './components/dataTables/books/books.component';
 import { EditBookComponent } from './components/dialogs/edit-book/edit-book.component';
 import { DeleteBookComponent } from './components/dialogs/delete-book/delete-book.component';
+import {MAT_CHECKBOX_CLICK_ACTION, MAT_CHECKBOX_DEFAULT_OPTIONS, MatCheckbox, MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -63,7 +64,9 @@ import { DeleteBookComponent } from './components/dialogs/delete-book/delete-boo
     MatListModule,
     MatSortModule,
     MatSnackBarModule,
-    MatMenuModule
+    MatMenuModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
