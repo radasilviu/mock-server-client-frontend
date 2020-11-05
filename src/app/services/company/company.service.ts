b9b95d92224645480abea630b6ec0818e90b9dc0
sebastian-sirb <sebastian.sirb@fortech.ro>
2020-11-05 10:52:21 +0200

    Refactored company service to take advantage of Entity service's implementations

--- src/app/services/company/company.service.ts
+++ src/app/services/company/company.service.ts
@@ -1,49 +1,25 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 import {Company} from '../../models/company';
 import {Env} from '../../configs/env';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {EntityService} from '../entity/entity.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CompanyService {
+export class CompanyService extends EntityService<Company>{
 
-  constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
-
-  list(limit: number, pageIndex: number, filter: string = '', columns: string[], sortColumn: string, sortDirection: string): any {
-    const data = {
-      pageSize : limit,
-      offset: pageIndex * limit,
-      searchTerm: filter,
-      columnsToSearchIn : columns,
-      sortColumn,
-      sortDirection
-    };
-    const url = Env.resourceServerRootURL + '/api/company/list';
-
-    return this.http.post(url, data)
-      .pipe(
-        catchError(error => {
-          return this.handleError(error, this.snackBar);
-        })
-      );
+  constructor(http: HttpClient, snackBar: MatSnackBar) {
+    super('/api/company',
+      http,
+      snackBar);
   }
 
-  delete(id: number): Observable<any> {
-    const url = Env.resourceServerRootURL + `/api/company/${id}`;
-
-    return this.http.delete(url)
-      .pipe(
-        catchError(error => {
-          return this.handleError(error, this.snackBar);
-        })
-      );
-  }
-
-  update(data: Company, id: number): Observable<any> {
+  // We can also overwrite methods, but for now we HAVE to overwrite this one, because the urls don't match
+  update(id: number, data: Company): Observable<any> {
     const url = Env.resourceServerRootURL + `/api/company/${id}`;
 
     return this.http.put(url, data)
@@ -52,13 +28,5 @@ export class CompanyService {
           return this.handleError(error, this.snackBar);
         })
       );
-  }
-
-  handleError(error: HttpErrorResponse, snackBar: MatSnackBar): Observable<never> {
-    console.error(error);
-    snackBar.open('Something bad happened, please try again later.', '', {
-      duration: 3000
-    });
-    return throwError(error.message);
   }
 }
